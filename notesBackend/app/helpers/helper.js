80ac26fec28c313b4059ecaeb881952b43f4454d
Usama Ahmad <usama@Usamas-MacBook-Pro.local>
2024-02-26 06:20:34 +0500

    reface: remove unused

--- notesBackend/app/helpers/helper.js
+++ notesBackend/app/helpers/helper.js
@@ -37,71 +37,7 @@ function decryptData(data) {
   console.log("dec===>", decryptedString)
   return decryptedString
 }
-
-const getTemplate = (type, data) => {
-  switch (type) {
-    case "addReview":
-      return reviewEmail(data)
-
-    default:
-      break
-  }
-}
-
-function sendEmail(to, type, data) {
-  let mailOptions = {
-    from: config.email,
-    to: "usama@adziv.com",
-    subject: data?.emailData?.subject ?? "Quoli Email",
-    text: null,
-    html: getTemplate(type, data),
-  }
-  console.log("mailOptions", getTemplate(type, data))
-  transporter.sendMail(mailOptions, function (err, info) {
-    if (err) console.log("err", err)
-    else console.log("info", info)
-  })
-}
-function getAverageRating(reviewStats, totalDocs) {
-  let sum = 0
-  reviewStats?.map((item) => {
-    sum += item._id * item.count
-  })
-  console.log("sum", reviewStats)
-  console.log("totalDocs", totalDocs)
-  console.log(
-    "sum",
-    isNaN((sum / totalDocs).toFixed(1) || 0)
-      ? 0
-      : (sum / totalDocs).toFixed(1) || 0
-  )
-  return isNaN((sum / totalDocs).toFixed(1) || 0)
-    ? 0
-    : isFinite((sum / totalDocs).toFixed(1) || 0)
-    ? (sum / totalDocs).toFixed(1) || 0
-    : 0
-}
-
-function getRating(rating, reviewStats, totalDocs) {
-  const value = isNaN(
-    parseFloat(
-      ((reviewStats?.filter((item) => item._id == rating)[0]?.count ?? 0) /
-        totalDocs) *
-        100
-    )
-  )
-    ? 0
-    : parseFloat(
-        ((reviewStats?.filter((item) => item._id == rating)[0]?.count ?? 0) /
-          totalDocs) *
-          100
-      )?.toFixed(2)
-  return value
-}
 module.exports = {
   encryptData,
   decryptData,
-  sendEmail,
-  getAverageRating,
-  getRating,
 }
